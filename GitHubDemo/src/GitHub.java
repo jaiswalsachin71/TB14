f021bbf7abc950bb6227c01b0db5625f2becf5ab
jaiswalsachin71 <104354479+jaiswalsachin71@users.noreply.github.com>
2022-04-25 16:57:28 +0530

    Update GitHub.java

--- GitHubDemo/src/GitHub.java
+++ GitHubDemo/src/GitHub.java
@@ -3,6 +3,10 @@ public class GitHub {
 	public void m1() {
 		System.out.println("m1 method");
 	}
+	
+	public void m2() {
+		System.out.println("m2 method");
+	}
 	public void branchCreate() {
 		System.out.println("Branch create method");
 	}
